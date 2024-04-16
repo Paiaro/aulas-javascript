90c821b950af2e0fb67a0c8d8f72f84a7443f8ec
Paiaro <gabrielpaiarop@gmail.com>
2024-04-16 16:46:40 -0300

    Update 02-arrays.js

--- js/02-arrays.js
+++ js/02-arrays.js
@@ -3,9 +3,9 @@
 const alunos = ["Wilian", "Vinicius", "Marli", "Duda", "Gabriel"];
 
 //exibindo a estrutura do array (somente no console)
- console.log(alunos);
+console.log(alunos);
 
- //Acessando individualmente alguns alunos atravez do índice
- console.log(alunos[1]); // Vinicius 
- console.log(alunos[3]); // Duda
- console.log(alunos[0]); // Wilian
+//Acessando individualmente alguns alunos atravez do índice
+console.log(alunos[1]); // Vinicius 
+console.log(alunos[3]); // Duda
+console.log(alunos[0]); // Wilian
