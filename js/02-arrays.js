f3395c5f4d303c50984c146386eec6ff9255d295
Paiaro <gabrielpaiarop@gmail.com>
2024-04-16 16:45:42 -0300

    Update 02-arrays.js

--- js/02-arrays.js
+++ js/02-arrays.js
@@ -1,3 +1,11 @@
 'use strict';
+//DECLARANDO UM ARRAY DE ALUNOS 
+const alunos = ["Wilian", "Vinicius", "Marli", "Duda", "Gabriel"];
 
-console.log("arrays");
+//exibindo a estrutura do array (somente no console)
+ console.log(alunos);
+
+ //Acessando individualmente alguns alunos atravez do índice
+ console.log(alunos[1]); //Vinicius 
+ console.log(alunos[3]); //Duda
+ console.log(alunos[0]); //Wilian
