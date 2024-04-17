48b7bc3035da294c9cf56aee01c0b29bd3938328
Paiaro <gabrielpaiarop@gmail.com>
2024-04-17 14:23:57 -0300

    Update 03-objetos.js

--- js/03-objetos.js
+++ js/03-objetos.js
@@ -1,2 +1,14 @@
 'use strict';
 
+// Declarando um objeto
+
+const livro = {
+
+    titulo: "Senhor dos Anéis", autor: "J.R.R. Tolkien", volumes: 3, ano: 1954
+
+};
+console.log(livro);
+
+console.log(livro.titulo);
+
+console.log(`${livro.titulo} foi criado por ${livro.autor}`);
