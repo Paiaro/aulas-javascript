0bfce77f53769896b06993b71ee349b043d2ce31
Paiaro <gabrielpaiarop@gmail.com>
2024-04-17 14:46:01 -0300

    Update 03-objetos.js

--- js/03-objetos.js
+++ js/03-objetos.js
@@ -12,3 +12,26 @@ console.log(livro);
 console.log(livro.titulo);
 
 console.log(`${livro.titulo} foi criado por ${livro.autor}`);
+
+
+// Exercicio 01 
+
+/*Crie um novo objeto chamado "carro" contendo as seguintes características:
+- marca do carro
+- modelo
+- ano de fabricação
+- quantidade de portas
+- cor
+-lista de opcionais (ar-condicionado, vidros elétricos, alarme)*/
+
+/* exercicio 02 
+
+mostre a segunte mensagem no console: 
+o fiat uno fabricado em 1996 com duas portas e com vidros elétricos, foi um famoso carro no brasil.*/
+
+const carro = {
+    marca: "Karl Benz", modelo: "Benz Patent-Motorwagen", ano: "1886", quantidadeportas: 2, cor: "preto", opcionais: ["ar-condicionado", "vidros eletricos", "alarme"]
+
+}
+
+console.log(`O ${carro.modelo} foi fabricado no ano de ${carro.ano} e com ${carro.quantidadeportas} e com ${carro.opcionais[1]} foi um carro famoso no mundo.`);
