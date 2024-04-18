1bd103343460529fba738b40d699e0a3913e844d
Paiaro <gabrielpaiarop@gmail.com>
2024-04-18 16:40:47 -0300

    Update loops.js

--- js/loops.js
+++ js/loops.js
@@ -1,3 +1,18 @@
 'use strict';
 
 
+/* Loops tradicionais mais usados:
+- while (enquanto)
+- for (para)
+
+
+Normalmente, os loops são controlandos através de uma variavél contadora ou de alguma condição lógica para que p loop seja interrompido.*/
+
+// Loop While
+
+let contador = 1;
+
+while (contador <= 5) {
+    console.log(`Valor do contador : ${contador}`);
+    contador++;
+}
