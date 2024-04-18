e271a20c36a912a1ff53010d218f30b1a2edced9
Paiaro <gabrielpaiarop@gmail.com>
2024-04-18 15:15:39 -0300

    Update operadores-logicos.js

--- js/operadores-logicos.js
+++ js/operadores-logicos.js
@@ -1,2 +1,22 @@
 'use strict';
 
+// Requisitos para aprovação 
+
+const mediaMinima = 7;
+const faltasMaxima = 10;
+
+// Aluno de exemplo
+
+let aluno = "Dayane";
+let mediaFinal = 6;
+let faltas = 15;
+let resultado;
+// operador && "and" ("E")
+if (mediaFinal >= mediaMinima && faltas <= faltasMaxima ){
+resultado = "aprovado";
+}
+else {
+    resultado = "reprovado";
+
+}
+console.log(resultado);
