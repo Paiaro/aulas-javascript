7e836d625013077414f002e07f1b322ffae88d58
Paiaro <gabrielpaiarop@gmail.com>
2024-04-18 14:19:08 -0300

    Update exercicio01.js

--- js/exercicio01.js
+++ js/exercicio01.js
@@ -6,10 +6,10 @@ let nota2 = 6;
 
 let media = (nota1 + nota2) / 2;
 console.log(media);
-if (media >= 5 ) {
+if (media >= 7 ) {
     console.log(`Aprovado!`);
 }
-else if (media <= 4) {
+else if (media >= 5) {
 
     console.log(`recuperação`);
 }
