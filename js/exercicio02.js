4324d1e7df4cf36adb56a793ee34143379307f3f
Paiaro <gabrielpaiarop@gmail.com>
2024-04-23 15:45:19 -0300

    loop

--- js/exercicio02.js
+++ js/exercicio02.js
@@ -4,8 +4,8 @@ const clientes = [
     { id: 1, name: 'Goku' },
     { id: 2, name: 'Naruto' },
     { id: 3, name: 'Shiryu' },
-  ];
-  
-  for (const cliente of clientes) {
+];
+
+for (const cliente of clientes) {
     console.log(`Cliente: ${cliente.name} (id: ${cliente.id})`);
-  }
+}
