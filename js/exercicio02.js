8495aca396023ead85013369a0756a94987d24e3
Paiaro <gabrielpaiarop@gmail.com>
2024-04-23 15:41:32 -0300

    exercicio 2

---
+++ js/exercicio02.js
@@ -0,0 +1,11 @@
+'use sctrict';
+
+const clientes = [
+    { id: 1, name: 'Goku' },
+    { id: 2, name: 'Naruto' },
+    { id: 3, name: 'Shiryu' },
+  ];
+  
+  for (const clientes of clientes) {
+    console.log(`Cliente: ${clientes.name} (id: ${cliente.id})`);
+  }
