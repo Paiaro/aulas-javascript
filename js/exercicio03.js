49f61c04b09618dd4033b2b062ee97b55f99cdef
Paiaro <gabrielpaiarop@gmail.com>
2024-04-24 13:55:02 -0300

    Update exercicio03.js

--- js/exercicio03.js
+++ js/exercicio03.js
@@ -1,23 +1,23 @@
 'use script';
 
-    function escrevaL() {
+function escrevaL() {
 
-        document.write("<br>");
-    }
+    document.write("<br>");
+}
 
-    function escreva(frase) {
+function escreva(frase) {
 
-        document.write(frase); 
-        escrevaL();
-    }
+    document.write(frase);
+    escrevaL();
+}
 
-    function converter(dolar) {
-        return dolar*5.16;
-    }
+function converter(dolar) {
+    return dolar * 5.16;
+}
 
-    let dolarInformado = parseFloat(prompt("qual a quantidade de dolar "));
-    let convertido = converter(dolarInformado);
+let dolarInformado = parseFloat(prompt("qual a quantidade de dolar "));
+let convertido = converter(dolarInformado);
 
-    escreva("Você irá ter" + " R$" + convertido + " reais");
-    escreva("E o equivalente a:" + " $" + dolarInformado + " dólares");
+escreva("Você irá ter" + " R$" + convertido + " reais");
+escreva("E o equivalente a:" + " $" + dolarInformado + " dólares");
 
