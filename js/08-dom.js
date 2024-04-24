968615bdaf73c8be6467455eaa20b001f93d83b5
Paiaro <gabrielpaiarop@gmail.com>
2024-04-24 16:33:06 -0300

    js

---
+++ js/08-dom.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/* Funções/métodos comuns para seleção de elementos no DOM
+
+-getElementById()
+Seleciona um elemento através do atributo ID
+
+- querySelector()
+Seleciona UM ELEMENTO através de seletor baseado na lógica CSS
+
+- querySelectorAll()
+ Seleciona TODOS ou Vários elementos através do seletor baseado na lógica CSS
+*/
