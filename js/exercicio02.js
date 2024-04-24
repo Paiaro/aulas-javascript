6e7f3e8198051f56971157d0844414fdd3991bf6
Paiaro <gabrielpaiarop@gmail.com>
2024-04-24 15:06:13 -0300

    exercicios

--- js/exercicio02.js
+++ js/exercicio02.js
@@ -1,11 +1,24 @@
-'use sctrict';
+'use strict';
 
 const clientes = [
-    { id: 1, name: 'Goku' },
-    { id: 2, name: 'Naruto' },
-    { id: 3, name: 'Shiryu' },
+    { id: 1, nome: "Goku" },
+    { id: 2, nome: "Naruto" },
+    { id: 3, nome: "Shiryu" }
 ];
 
-for (const cliente of clientes) {
-    console.log(`Cliente: ${cliente.name} (id: ${cliente.id})`);
+// Versão usando loop for
+
+// Armazenando em cache (memória) o tamanho do array antes de iniciar o loop
+let quantidade = clientes.length; // bom para perfomance!
+
+for( let i = 0; i < quantidade; i++ ){
+    console.log(`Cliente: ${clientes[i].nome} (id: ${clientes[i].id})`);
+}
+
+console.log("--------------");
+
+// Versão usando for/of
+/* Acessando 'cada cliente' (que é objeto) DENTRO do array 'clientes' */
+for( let cliente of clientes ){
+    console.log(`Cliente: ${cliente.nome} (id: ${cliente.id})`);
 }
--- js/exercicio03.js
+++ js/exercicio03.js
@@ -1,23 +1,34 @@
 'use script';
 
-function escrevaL() {
+// function escrevaL() {
 
-    document.write("<br>");
+//     document.write("<br>");
+// }
+
+// function escreva(frase) {
+
+//     document.write(frase);
+//     escrevaL();
+// }
+
+// function converter(dolar) {
+//     return dolar * 5.16;
+// }
+
+
+// let dolarInformado = parseFloat(prompt("qual a quantidade de dolar ?"));
+// let convertido = converter(dolarInformado);
+
+// escreva("Você irá ter" + " R$ "+convertido+" reais.");
+// escreva(`é equivalente a ${dolarInformado} dolares.`);
+
+let dolar = 5.16;
+let valorDolar = 1000;
+
+
+function converter (valor){
+
+   return valor * dolar;
+
 }
-
-function escreva(frase) {
-
-    document.write(frase);
-    escrevaL();
-}
-
-function converter(dolar) {
-    return dolar * 5.16;
-}
-
-let dolarInformado = parseFloat(prompt("qual a quantidade de dolar ?"));
-let convertido = converter(dolarInformado);
-
-escreva("Você irá ter" + " R$"+convertido+" reais.");
-escreva(`é equivalente a ${dolarInformado} dolares.`);
-
+console.log(converter(valorDolar));
