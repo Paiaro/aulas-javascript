3aae035526a507db8f9aef961129b8a82546a04b
Paiaro <gabrielpaiarop@gmail.com>
2024-04-24 15:29:31 -0300

    Update exercicio03.js

--- js/exercicio03.js
+++ js/exercicio03.js
@@ -22,13 +22,36 @@
 // escreva("Você irá ter" + " R$ "+convertido+" reais.");
 // escreva(`é equivalente a ${dolarInformado} dolares.`);
 
+
+
+
+
+// versão corrigida
 let dolar = 5.16;
 let valorDolar = 1000;
 
 
-function converter (valor){
+function converter(valor) {
 
-   return valor * dolar;
+    return valor * dolar;
 
 }
+
+function formatarMoeda(valor, moedas) {
+    const opcoes = {
+        style: "currency",
+        currency: moedas
+    };
+    return new Intl.NumberFormat("pt-br", opcoes).format(valor);
+
+}
+// versão sem fomatação
 console.log(converter(valorDolar));
+//versão 1 (função com parametro para outra função) com formatação
+console.log(formatarMoeda(converter(valorDolar), "BRL"));
+
+// segunda versão corrigida (guardamos o resultado da conversão; depois passamos o resultado para a formatação)
+
+let resultadoReais = converter(valorDolar);
+
+console.log(formatarMoeda(valorDolar,"USD"));
