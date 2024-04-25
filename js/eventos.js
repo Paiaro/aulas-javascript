080dc7acd18bfa1af4524218ac293209e11f1354
Paiaro <gabrielpaiarop@gmail.com>
2024-04-25 15:58:02 -0300

    Update eventos.js

--- js/eventos.js
+++ js/eventos.js
@@ -10,6 +10,10 @@ const pagina = document.querySelector("body");
 const efeitoSom = new Audio();
 efeitoSom.src = "wow.wav";
 
+//Criando um objeto de imagem referenciando o arquivo
+ 
+const imagem = new Image();
+imagem.src = "meme.fig";
 
 // Criar um ouvinte de eventos (Event Listener)
 //O primeiro parâmetro do addEventoListener é o tipo de evento que queremos "ouvir/monitorar". O segundo parametro é uma função (conhecida como callback) responsavel por executar ações no momento em que o evento acontecer.
