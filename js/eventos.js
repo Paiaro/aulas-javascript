99d208361f2e84834ec0f7cda1f8acccdc926939
Paiaro <gabrielpaiarop@gmail.com>
2024-04-25 15:04:35 -0300

    Update eventos.js

--- js/eventos.js
+++ js/eventos.js
@@ -4,5 +4,8 @@
 const exemplo01 = document.querySelector("#exemplo01");
 
 // Criar um ouvinte de eventos (Event Listener)
+//O primeiro parâmetro do addEventoListener é o tipo de evento que queremos "ouvir/monitorar". O segundo parametro é uma função (conhecida como callback) responsavel por executar ações no momento em que o evento acontecer.
+exemplo01.addEventListener("click", function(){
+    alert("voce apertou !");
+});
 
-exemplo01.addEventListener("");
