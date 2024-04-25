44e1085c1c84b2ad68aedab004d747ddfaad63b6
Paiaro <gabrielpaiarop@gmail.com>
2024-04-25 14:52:39 -0300

    Update eventos.js

--- js/eventos.js
+++ js/eventos.js
@@ -1,1 +1,8 @@
 'use strict';
+
+//Seleção dos elementos
+const exemplo01 = document.querySelector("#exemplo01");
+
+// Criar um ouvinte de eventos (Event Listener)
+
+exemplo01.addEventListener("");
